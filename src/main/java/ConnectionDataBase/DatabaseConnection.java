9ba347f5636aab7176eb73bfa35c902717497b41
vitor gabriel araujo borges <vitor.desenvolvedor18@gmail.com>
2026-06-30 15:11:04 -0300

    Atualizar o DatabaseConnection.java

--- src/main/java/ConnectionDataBase/DatabaseConnection.java
+++ src/main/java/ConnectionDataBase/DatabaseConnection.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 
 public class DatabaseConnection {
-    public static final String URL = "jdbc:mysql://localhost:3306/empresa_db";
+    public static final String URL = "jdbc:mysql:******";
     public static final String USER = "root";
-    public static final String PASSWORD = "Vitor2008/";
+    public static final String PASSWORD = "******";
 
     public static Connection getConnection()  throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
