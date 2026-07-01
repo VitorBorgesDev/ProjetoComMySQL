de4302bde6541df94c776773a3fd0af513afff67
Vtor Gabriel <Vitor.desenvolvedor18@gmail.com>
2026-07-01 00:35:58 -0300

    fix: remove credenciais do banco do controle de versao

--- src/main/java/ConnectionDataBase/DatabaseConnection.java
+++ src/main/java/ConnectionDataBase/DatabaseConnection.java
@@ -1,22 +1,40 @@
 package ConnectionDataBase;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
-
+import java.util.Properties;
 
 public class DatabaseConnection {
-    public static final String URL = "jdbc:mysql://localhost:3306/empresa_db";
-    public static final String USER = "root";
-    public static final String PASSWORD = "Vitor2008/";
 
-    public static Connection getConnection()  throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+    private static final Properties props = new Properties();
 
+    static {
+        try (InputStream input = DatabaseConnection.class
+                .getClassLoader()
+                .getResourceAsStream("db.properties")) {
 
+            if (input == null) {
+                throw new RuntimeException("Arquivo db.properties não encontrado em resources!");
+            }
+            props.load(input);
+
+        } catch (IOException e) {
+            throw new RuntimeException("Erro ao carregar db.properties", e);
+        }
     }
 
+    public static Connection getConnection() throws SQLException {
+        String url = props.getProperty("db.url");
+        String user = props.getProperty("db.user");
+        String password = props.getProperty("db.password");
 
-
-
+        return DriverManager.getConnection(url, user, password);
+    }
 }
+
+
+
+
